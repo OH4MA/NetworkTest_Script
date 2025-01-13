28faca553de09993945fe878ff9da609159ae329
ㄌㄇㄎ <165235010+Oh4ma@users.noreply.github.com>
2025-01-13 11:52:32 +0800

    Update networktest.ps1

--- networktest.ps1
+++ networktest.ps1
@@ -2,7 +2,7 @@
 
 # 檢查 Python 是否安裝
 if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Host "Python沒有安裝，可以按照以下指令進行安裝："
+    Write-Host "Python沒有安裝，可以按照以下方法進行安裝："
     Write-Host "1. 前往 https://www.python.org/downloads/ 下載並安裝最新版本的 Python。"
     Write-Host "2. 確保在安裝過程中勾選 'Add Python to PATH' 選項。"
     exit 1
@@ -34,7 +34,7 @@ if (-not (python -m pip show speedtest-cli -ErrorAction SilentlyContinue)) {
     Write-Host "speedtest-cli已安裝"
 }
 
-# 執行 networktest.py
+# 執行 python測試網路腳本
 $URL = "https://raw.githubusercontent.com/Oh4ma/NetworkTest_Script/main/networktest.py"
 
 try {
